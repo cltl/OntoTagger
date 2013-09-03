f44e1d773f43db50dd4829fe38ab8aacf6e21bba
piekvossen <piek.vossen@vu.nl>
2013-09-03 12:13:53 +0200

    - Adapted for predicate matrix version 1 which has ILI-synset ids. It now takes any specified synsets into account, otherwise it adds the synsets

---
+++ src/main/java/eu/kyotoproject/sound/StraatGeluiden.java
@@ -0,0 +1,11 @@
+package eu.kyotoproject.sound;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kyoto
+ * Date: 9/3/13
+ * Time: 12:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StraatGeluiden {
+}
