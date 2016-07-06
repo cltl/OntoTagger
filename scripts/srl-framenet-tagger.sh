389914ef2b563a7f3b9428ae0376665e5b40b292
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-07-06 02:56:01 +0200

    Get version from pom (fixes #6)

--- scripts/srl-framenet-tagger.sh
+++ scripts/srl-framenet-tagger.sh
@@ -1,8 +1,11 @@
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$( cd $DIR && cd .. && pwd)"
 LIB="$ROOT"/lib
+VERSION=`python $DIR/pom_version.py`
 
 #pass naf file as input stream and catch the naf  output stream
 # for example> "cat example-naf.xml | srl-framenet.sh > naf.srl-fn.xml"
 
-java -Xmx1812m -cp "$LIB/ontotagger-v3.0-jar-with-dependencies.jar" eu.kyotoproject.main.SrlFrameNetTagger --frame-ns "fn:" --role-ns "fn-role:;pb-role:;fn-pb-role:;eso-role:" --ili-ns "mcr:ili" --sense-conf 0.05 --frame-conf 30 
+java -Xmx1812m -cp "$LIB/ontotagger-$VERSION-jar-with-dependencies.jar" eu.kyotoproject.main.SrlFrameNetTagger --frame-ns "fn:" --role-ns "fn-role:;pb-role:;fn-pb-role:;eso-role:" --ili-ns "mcr:ili" --sense-conf 0.05 --frame-conf 30 
