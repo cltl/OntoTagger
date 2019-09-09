be2ff1d4d673379d68c5cd1664160d177f2cd253
piekvossen <piek.vossen@vu.nl>
2019-09-09 16:37:44 +0200

    -- add PredicateMatrix concepts to event coreference layer on the basis of the represented senses

--- scripts/predicate-matrix-tagger.sh
+++ scripts/predicate-matrix-tagger.sh
@@ -9,4 +9,4 @@ RESOURCES="$( cd $ROOT && cd .. && pwd)"/vua-resources
 #pass naf file as input stream and catch the naf  output stream
 # for example> "cat example-naf.xml | predicate-matrix.sh > naf.pm.xml"
 
-java -Xmx1812m -cp "$LIB/ontotagger-v3.1.1-jar-with-dependencies.jar" eu.kyotoproject.main.KafPredicateMatrixTagger --mappings "fn:;;ili;eso" --ili --predicate-matrix "$RESOURCES/PredicateMatrix.v1.3.txt.role.odwn.gz" --grammatical-words "$RESOURCES/Grammatical-words.en"
+java -Xmx1812m -cp "$LIB/ontotagger-v3.1.1-jar-with-dependencies.jar" eu.kyotoproject.main.KafPredicateMatrixTagger --mappings "fn:;ili;eso" --ili --predicate-matrix "$RESOURCES/PredicateMatrix.v1.3.txt.role.odwn.gz" --grammatical-words "$RESOURCES/Grammatical-words.en"
